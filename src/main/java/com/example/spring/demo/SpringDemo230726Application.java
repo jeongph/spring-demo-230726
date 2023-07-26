6e392ac2f2f248899afb7099818de6bf9cb051eb
JeongUk Park <dnr1105@gmail.com>
2023-07-26 23:07:57 +0900

    init:

---
+++ src/main/java/com/example/spring/demo/SpringDemo230726Application.java
@@ -0,0 +1,13 @@
+package com.example.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo230726Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemo230726Application.class, args);
+    }
+
+}
